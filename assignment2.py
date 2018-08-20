deee6a9ef96ce2200a996609f8e623e869b24e2c
Anmol kalra <42096235+anmolkalra@users.noreply.github.com>
2018-08-20 18:45:23 +0530

    assigment-2

---
+++ assignment2.py
@@ -0,0 +1,30 @@
+#1
+print("My name is Anmol kalra")
+
+#2
+str1 = "Anmol"
+str2 = "kalra"
+print(str1+str2)
+
+#3
+x = int(input("enter value for x "))
+y = int(input("enter value for y "))
+z = int(input("enter value for z "))
+print("x=",x," y =",y," z=",z)
+
+#4
+print("let's get started")
+
+#5
+s = "Acadview"
+course = "Python"
+fees = 5000
+str1 = "{} {} course fee is {}".format(s,course,fees)
+print(str1)
+
+#6
+
+pi=3.14
+r=float(input("Enter Radius of circle"))
+area=pi*(r**2)
+print("Area of circle:- {}".format(area))
