d258125964e59b29fc99d5438dae938d6cb7d102
Anmol kalra <42096235+anmolkalra@users.noreply.github.com>
2018-08-28 19:16:17 +0530

    file handling

---
+++ filehandling.py
@@ -0,0 +1,67 @@
+
+
+#1
+f=open('test.txt')
+lines=f.readlines()
+print(lines)
+c=int(input("enter n"))
+for i in range(c):
+    print(lines[i])
+f.close()
+#2
+a=[]
+dict1={}
+
+f=open('test.txt')
+data=f.read()
+print(data)
+
+
+a=(data.split())
+for i in a:
+    dict1[i]=a.count(i)
+print(dict1)
+f.close()
+#3
+f=open('test.txt')
+g=open('test1.txt','w')
+
+data=f.read()
+g.write(data)
+g.close()
+
+g=open('test1.txt')
+d=g.read()
+print(d)
+g.close()
+f.close()
+#4
+f=open('test.txt')
+g=open('test1.txt')
+data=f.read()
+ty=g.read()
+print(ty)
+a=data.split()
+b=ty.split()
+
+for i in range(len(a)):
+    print(a[i]+b[i])
+g.close()
+f.close()
+#5
+b=[]
+f=open('test.txt','w')
+for i in range(10):
+    a=input('enter element')
+    f.write(a)
+    f.write('\n')
+f.close()
+f=open('test.txt')
+data=f.read()
+t=data.split()
+print(t)
+for i in t:
+    b.append(int(i))
+b.sort()
+print(b)
+
